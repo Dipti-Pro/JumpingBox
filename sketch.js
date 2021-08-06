fe7279c1bc0bffe48bb1b8bb6ea25504581b8cb9
Dipti-Pro <85604091+Dipti-Pro@users.noreply.github.com>
2021-08-06 17:03:29 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -40,20 +40,7 @@ function draw() {
         music.play();
     }
 
-    if(block2.isTouching(ball)){
-        ball.shapeColor = rgb(255,128,0);
-        ball.velocityX = 0;
-        ball.velocityY = 0;
-        music.stop();
-    }
 
-    if(block3.isTouching(ball) && ball.bounceOff(block3)){
-        ball.shapeColor = rgb(153,0,76);
-    }
-
-    if(block4.isTouching(ball) && ball.bounceOff(block4)){
-        ball.shapeColor = rgb(0,100,0);
-    }
 
     drawSprites();
 }
